ca25b02f3b00e8b701f00aa7d3725002d4cb6a8c
Ana Lara Ribeiro Sodré <135715203+kindhrt@users.noreply.github.com>
2024-11-01 14:20:09 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,7 @@
+// perguntas.js
+const perguntas = [
+    { categoria: "Programação", pergunta: "O que é Python?", resposta: "Python é uma linguagem de programação." },
+    { categoria: "Geografia", pergunta: "Qual é a capital da França?", resposta: "A capital da França é Paris." },
+    { categoria: "Programação", pergunta: "O que é uma função?", resposta: "Uma função é um bloco de código que executa uma tarefa." },
+    { categoria: "Língua Inglesa", pergunta: "Como se diz 'oi' em inglês?", resposta: "Oi em inglês é 'Hi' (pronuncia-se 'rai')." }
+];
